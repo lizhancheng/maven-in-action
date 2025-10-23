98b70fcee6b3d4c183ed208877a5f1ce4e6fec24
lizhancheng <279623463@qq.com>
2025-10-23 21:34:30 +0800

    wip: learning SAX parse

--- src/main/java/com/app/document/DomParser.java
+++ src/main/java/com/app/document/DomParser.java
@@ -4,24 +4,31 @@ import java.io.InputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class DomParser {
 	public static void main(String[] args) throws Exception {
-		InputStream inputStream = DomParser.class.getResourceAsStream("/book.xml");
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-		Document document = documentBuilder.parse(inputStream);
-
-		Element root = document.getDocumentElement();
-		traverse(root, 0);
+		// new DOM();
+		new SAX();
 	}
+}
 
+class DOM {
+	public DOM() {
+		executeParse();
+	}
+	
 	private static void traverse(Node node, int depth) {
 
 		String indent = StringUtils.repeat("  ", depth);
@@ -53,5 +60,71 @@ public class DomParser {
 		for (int i = 0; i < children.getLength(); i ++) {
 			traverse(children.item(i), depth + 1);
 		}
+	}
+	
+	public void executeParse() {
+		try {
+			InputStream inputStream = DomParser.class.getResourceAsStream("/book.xml");
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			Document document = documentBuilder.parse(inputStream);
+			
+			Element root = document.getDocumentElement();
+			traverse(root, 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+// 基于流的解析方式，边读取XML边解析，并以事件回调方式让调用者获取数据
+// 因可一边读一边解析，所以不管XML多大，占用的内存都很小
+class SAX {
+	public SAX() {
+		executeParse();
+	}
+
+	public void executeParse() {
+		InputStream inputStream = DomParser.class.getResourceAsStream("/book.xml");
+		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+		try {
+			SAXParser saxParser = saxParserFactory.newSAXParser();
+			saxParser.parse(inputStream, new Handler());
+		} catch (Exception exp) {
+			exp.printStackTrace();
+		}
+	}
+	
+	class Handler extends DefaultHandler {
+		void print(Object... objs) {
+			for (Object obj : objs) {
+				System.out.print(obj + " ");
+			}
+			System.out.println();
+		}
+		public void startDocument() throws SAXException {
+			print("start document");
+		}
+		
+		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+			print("start element:", localName, qName);
+		}
+		
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			print("end element:", localName, qName);
+		}
+		
+		public void characters(char[] ch, int start, int length) throws SAXException {
+			print("characters:", new String(ch, start, length));
+		}
+		
+		public void error(SAXParseException e) throws SAXException {
+			print("error:", e);
+		}
+		
+		public void endDocument() throws SAXException {
+			print("end document");
+		}
+		
 	}
 }
