cef15d5396322270b5f270f613e1d9f3526c4465
lizhancheng <279623463@qq.com>
2025-10-27 21:37:38 +0800

    wip: learning JDBC base knowledge

--- src/main/java/com/app/database/Jdbc.java
+++ src/main/java/com/app/database/Jdbc.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Random;
 
 public class Jdbc {
 	public static void main(String[] args) {
@@ -18,12 +19,16 @@ public class Jdbc {
 	static String jDBC_PASSWORD = "123456";
 	static String getAllUsersSql = "SELECT id, address, age, name FROM user;";
 	static String getConghuaUser = "SELECT * FROM user WHERE address = ?";
+	static String createUser = "INSERT INTO user (name, first_name, last_name, age, occupation, address, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
+	static String updateUser = "UPDATE user SET age=?, address=?, occupation=? WHERE id=?";
 	
 	public static void connectDatabase() {
 		try (Connection innerConnection = DriverManager.getConnection(JDBC_URL, JDBC_USER, jDBC_PASSWORD)) {
 			connection = innerConnection;
-			getAllUsers();
-			getSpecificUser();
+			// getAllUsers();
+			// getSpecificUser();
+			// insertRandomData();
+			updateUserInfo();
 		} catch (SQLException exp) {
 			exp.printStackTrace();
 		}
@@ -61,4 +66,100 @@ public class Jdbc {
 			exp.printStackTrace();
 		}
 	}
+	
+	public static String generateRandomString(int length, int middle) {
+		String name = "";
+		for (int i = 0; i < length; i ++) {
+			name += i != 0 ? i == middle ? "." : (char) (97 + 25 * Math.random()) : (char) (97 + 25 * Math.random()) ;
+		}
+		return name;
+	}
+	
+	public static String getRandomCity() {
+		String[] cities = new String[]{ "conghua", "baiyun", "tianhe", "haizhu", "yuexiu", "liwan" };
+		Random rd = new Random();
+		
+		int randomIndex = rd.nextInt(5);
+		return cities[randomIndex];
+	}
+	
+	public static String getRandomOccupation() {
+		String[] occupations = new String[] { "teacher", "player", "policy" };
+		Random rd = new Random();
+		
+		return occupations[rd.nextInt(2)];
+	}
+	
+	public static int getRandomAge() {
+		return (int) Math.round(Math.random() * 100);
+	}
+	
+	public static void insertRandomData() {
+		String name = generateRandomString(7, 3);
+		String firstName = name.substring(name.indexOf(".") + 1, 7);
+		String lastName = name.substring(0, name.indexOf("."));
+		int age = getRandomAge();
+		String occupation = getRandomOccupation();
+		String address = getRandomCity();
+		String password = generateRandomString(10, 0);
+		
+		try (PreparedStatement preparedStatement = connection.prepareStatement(createUser, Statement.RETURN_GENERATED_KEYS)) {
+			preparedStatement.setObject(1, name);
+			preparedStatement.setObject(2, firstName);
+			preparedStatement.setObject(3, lastName);
+			preparedStatement.setObject(4, age);
+			preparedStatement.setObject(5, occupation);
+			preparedStatement.setObject(6, address);
+			preparedStatement.setObject(7, password);
+			
+			int n = preparedStatement.executeUpdate();
+			System.out.println(n == 1 ? "Insert succeed! " : "Failed insertion.");
+			try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+				if (resultSet.next()) {
+					long id = resultSet.getLong(1);
+					System.out.println("Primary key: " + id);
+				}
+			}
+		} catch (SQLException exp) {
+			exp.printStackTrace();
+		}
+	}
+	
+	public static void updateUserInfo() {
+		int age = getRandomAge();
+		String address = getRandomCity();
+		String occupation = getRandomOccupation();
+		
+		try (PreparedStatement preparedStatement = connection.prepareStatement(updateUser, Statement.RETURN_GENERATED_KEYS)) {
+			preparedStatement.setObject(1, age);
+			preparedStatement.setObject(2, address);
+			preparedStatement.setObject(3, occupation);
+			preparedStatement.setObject(4, 12);
+			
+			int n = preparedStatement.executeUpdate();
+			if (n == 1) {
+				System.out.println("Update succeed!");
+				try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+					if (resultSet.next()) {
+						System.out.println(resultSet.getString("name"));
+					}
+				}
+			} else {
+				System.out.println("Failed update.");
+			}
+		} catch (SQLException exp) {
+			exp.printStackTrace();
+		}
+	}
+}
+
+class User {
+	long id;
+	String name;
+	String first_name;
+	String last_name;
+	String occupation;
+	int age;
+	String address;
+	String password;
 }
